630f470c5523b3ceb73e7c5d5aa108aed2445661
Juan Estrella <juan.estrella137@gmail.com>
2020-07-16 09:55:36 +0200

    Add child processes module to start 2nd program from node

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const program = require('caporal');
 const fs = require('fs');
+const { spawn } = require('child_process');
 
 program
   .version('0.0.1')
@@ -17,7 +18,7 @@ program
     }
 
     const start = debounce(() => {
-      console.log('STARTING USERS PROGRAM');
+      spawn('node', [ name ], { stdio: 'inherit' });
     }, 100);
 
     chokidar.watch('.').on('add', start).on('change', start).on('unlink', start);
