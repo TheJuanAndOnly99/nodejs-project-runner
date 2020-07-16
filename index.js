3d9ab219a22153ffc5579261d32af9d78e323c75
Juan Estrella <juan.estrella137@gmail.com>
2020-07-16 11:21:20 +0200

    Kill inital process when rerunning files

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const chokidar = require('chokidar');
 const program = require('caporal');
 const fs = require('fs');
 const { spawn } = require('child_process');
+const chalk = require('chalk');
 
 program
   .version('0.0.1')
@@ -17,8 +18,13 @@ program
       throw new Error(`Could not find the file ${name}`);
     }
 
+    let proc;
     const start = debounce(() => {
-      spawn('node', [ name ], { stdio: 'inherit' });
+      if (proc) {
+        proc.kill();
+      }
+      console.log(chalk.green('>>>>> Starting process...'));
+      proc = spawn('node', [ name ], { stdio: 'inherit' });
     }, 100);
 
     chokidar.watch('.').on('add', start).on('change', start).on('unlink', start);
